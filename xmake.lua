1ff7bb08f7d7c731b8c877c54d1a22acfb59faff
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-17 22:20:03 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -340,5 +340,5 @@ target("aria2c")
     after_build(function (target)
         os.mkdir("dist")
         local ext = is_plat("windows") and ".exe" or ""
-        os.cp(target:targetfile(), format("dist/aria2c0, ext))
+        os.cp(target:targetfile(), "dist/aria2c0.exe")
     end)
