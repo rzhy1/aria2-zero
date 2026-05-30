874e86126f5f3e3c120f57a68cc85af3bdb938ba
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-05-30 23:20:57 +0800

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,28 +1,27 @@
 set_policy("package.install_only", true)
+
 add_repositories("zeromake https://github.com/rzhy1/xrepo.git")
-local ssl_name = get_config("use_quictls") and "quictls" or "libressl"
+
 add_requires(
     "expat",
     "zlib",
     "sqlite3",
     "c-ares",
-    "nonstd.string-view"
+    "nonstd.string-view",
+    "boost.intl"
 )
-add_requires("boost.intl")
-if get_config("use_quictls") then
-    add_requires("quictls")
-    add_requires("ssh2", {configs = {quictls = true}})
-else
-    add_requires("libressl")
-    add_requires("ssh2", {configs = {libressl = true}})
-end
+
+local ssl_name = get_config("use_quictls") and "quictls" or "libressl"
+add_requires(ssl_name, {configs = {asm = false}})
+add_requires("ssh2", {configs = {[ssl_name] = true}})
+
 if get_config("unit") then
     add_requires("cppunit", {optional = true})
 end
+
 if os.host() == "windows" then
     add_requires("gettext-tools", {optional = true})
 end
-
 
 if get_config("with_breakpad") then
     add_requires("breakpad")
