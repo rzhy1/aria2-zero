24142e410315ecfbb94c33fd40fed18463acfe44
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-06-16 12:51:42 +0800

    Update package.lua

--- package.lua
+++ package.lua
@@ -23,18 +23,13 @@ elseif ssl_provider == "quictls" then
 elseif ssl_provider == "libressl" then
     add_requires("libressl")
 elseif ssl_provider == "wintls" then
-    -- 【修改】不再引入任何外部加密库（原 add_requires("libressl") 已删除）
+    add_requires("libressl")
 end
 
 -- 3. 配置 libssh2 依赖
 if is_plat("macosx", "iphoneos") and not ssl_external then
     add_requires("libssh2", {configs = {crypto = "securetransport"}})
-elseif ssl_provider == "wintls" then
-    -- 【新增】纯 wintls 模式下使用 Windows 原生 CNG 加密后端
-    add_requireconfs("libssh2", {install = "source"})
-    add_requires("libssh2", {configs = {crypto = "wincng"}})
 else
-    -- 其他所有外部 SSL 模式（openssl / quictls / libressl）使用 openssl 后端
     add_requireconfs("libssh2", {install = "source"})
     add_requires("libssh2", {configs = {crypto = "openssl"}})
 end
