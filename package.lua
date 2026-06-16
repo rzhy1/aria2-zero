98a123ec409274ef567f5dd6b94f9ab0497151ab
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-06-16 16:37:33 +0800

    Update package.lua

--- package.lua
+++ package.lua
@@ -17,8 +17,8 @@ local ssl_external = (ssl_provider ~= "wintls")
 
 -- 2. 根部显式引入对应的依赖包
 if ssl_provider == "openssl" then
-    --add_requires("openssl3 3.6.2") 
-    add_requires("openssl") 
+    add_requires("openssl3") 
+    --add_requires("openssl3 3.6.2")  
 elseif ssl_provider == "quictls" then
     add_requires("quictls")
 elseif ssl_provider == "libressl" then
